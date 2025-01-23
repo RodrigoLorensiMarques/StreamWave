56b6cd5b14de784dd8309784e00350c3baa8d965
Rodrigo Marques <rodrigolorensimarques@protonmail.com>
2025-01-23 10:38:24 -0300

    feat: adiciona docker-compose e Dockerfile para conternização

--- WebApi/obj/Debug/net9.0/WebApi.AssemblyInfo.cs
+++ WebApi/obj/Debug/net9.0/WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fb67d2318d2f5f041527d6381a84b5e9cd1ba49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbeb4435df35c9253a1569dc3c75a8ee394ff0c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
