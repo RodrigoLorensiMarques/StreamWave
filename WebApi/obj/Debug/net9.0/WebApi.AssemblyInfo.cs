93fa78730cac9b80b8c45c926f8ef67b3fd90edd
Rodrigo Marques <rodrigolorensimarques@protonmail.com>
2025-01-24 17:18:26 -0300

    fix: atualiza URL para container do nginx e ajusta volumes no docker-compose

--- WebApi/obj/Debug/net9.0/WebApi.AssemblyInfo.cs
+++ WebApi/obj/Debug/net9.0/WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2adbdbaf39950e11cda0096030ef1b6bda230fab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ce8923596a492d0e6cc334353078e93c0798d0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
